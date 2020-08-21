11417e3da26b8ff4673a3e4049401453027e640c
i-am-AV <i-am-AV@yandex.ru>
2020-08-21 13:15:00 +0300

    Initial Commit

---
+++ SN.Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SN.Messenger
+//
+//  Created by Alex Vasilyev on 21.08.2020.
+//  Copyright © 2020 Alex Vasilyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
