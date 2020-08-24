f508864f283a95f0318934d889099f8b3f5cbcd1
i-am-AV <i-am-AV@yandex.ru>
2020-08-24 13:09:01 +0300

    Added image caching

--- SN.Messenger/Helper/WebImageView.swift
+++ SN.Messenger/Helper/WebImageView.swift
@@ -12,13 +12,34 @@ final class WebImageView: UIImageView {
     func set(imageURL: String) {
         guard let url = URL(string: imageURL) else { return }
         
+        if let cacheResponse = URLCache.shared.cachedResponse(for: URLRequest(url: url)) {
+            self.image = UIImage(data: cacheResponse.data)
+            print("Loaded from cache")
+            return
+        }
+        
+        print("Loaded from internet")
+        
         let dataTask = URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
             DispatchQueue.main.async {
-                if let data = data {
+                if let data = data, let response = response {
                     self?.image = UIImage(data: data)
+                    self?.handleLoadedImage(data: data, response: response)
                 }
             }
         }
         dataTask.resume()
+    }
+    
+    /**
+        Сохранение изображения в кеш
+     - parameter data:
+     - parameter response:
+     
+     */
+    private func handleLoadedImage(data: Data, response: URLResponse) {
+        guard let responseURL = response.url else { return }
+        let cachedResponse = CachedURLResponse(response: response, data: data)
+        URLCache.shared.storeCachedResponse(cachedResponse, for: URLRequest(url: responseURL))
     }
 }
